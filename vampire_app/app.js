2f5282c1d33f650d80c114adcfecb1b7e6b8f72e
cooperama <coope133@gmail.com>
2020-10-03 17:25:14 -0700

    Commit 2 - Added data into vampire collection

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -2,12 +2,13 @@
 const mongoose = require('mongoose');
 
 // 2. Require your Model
-const Vampire = require('Vampire');
+const Vampire = require('./models/Vampire');
 
 // 3. Require your extra data source
-
+const vampireObjects = require('./populateVampires');
 
 // 4. Connect your database
+const db = require('./models');
 
 /////////////////////////////////////////////////
 //Write your answers to add, query, update, remove, and Hungry for More below.
@@ -16,8 +17,93 @@ const Vampire = require('Vampire');
 /////////////////////////////////////////////////
 // INSERT USING MONGOOSE
 // ### Add the vampire data that we gave you
+// Vampire.collection.insertMany(vampireObjects, (error, vampData) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(vampData);
+//   }
+//   mongoose.connection.close();
+// })
+
 
 // ### Add some new vampire data
+// db.Vampire.create({
+//   name: 'Viago',
+//   hair_color: 'brown',
+//   eye_color: 'brown',
+//   dob: 1641-07-15,
+//   loves: ['Katherine', 'cleanliness'],
+//   location: 'New Zealand',
+//   gender: 'male',
+//   victims: 642
+// }, (error, newVamp) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(newVamp);
+//   }
+//   process.exit();
+// })
+
+
+// db.Vampire.create({
+//   name: 'Nadja',
+//   hair_color: 'brown',
+//   eye_color: 'brown',
+//   dob: 1447-03-22,
+//   loves: ['Laszlo', 'orgies'],
+//   location: 'Staten Island',
+//   gender: 'female',
+//   victims: 1076
+// }, (error, newVamp) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(newVamp);
+//   }
+//   process.exit();
+// })
+
+
+// db.Vampire.create({
+//   name: 'Vladislav',
+//   hair_color: 'brown',
+//   eye_color: 'brown',
+//   dob: 1202-09-09,
+//   loves: ['ladies', 'poking'],
+//   location: 'New Zealand',
+//   gender: 'male',
+//   victims: 1846
+// }, (error, newVamp) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(newVamp);
+//   }
+//   process.exit();
+// })
+
+
+// db.Vampire.create({
+//   name: 'Colin Robinson',
+//   hair_color: 'bald',
+//   eye_color: 'blue',
+//   dob: 1978-05-12,
+//   loves: ['raining on parades', 'the DMV'],
+//   location: 'Staten Island',
+//   gender: 'male',
+//   victims: 642
+// }, (error, newVamp) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(newVamp);
+//   }
+//   process.exit();
+// })
+
+
 
 /////////////////////////////////////////////////
 // ## QUERYING
---
+++ vampire_app/models/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const connectionString = 'mongodb://localhost:27017/vampires';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log("MongoDB successfully connected ~~~");
+});
+
+mongoose.connection.on('error', (error) => {
+  console.log('MongoDB connection error: ', error);
+});
+
+
+module.exports = {
+  Vampire: require('./Vampire')
+};
